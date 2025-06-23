9ae6be2295b666a0a7baf74d3161273aa124b304
gererock <gererock@users.noreply.github.com>
2025-06-23 09:34:58 -0300

    aaa

--- src/tpiproyecto2/TPIPROYECTO2.java
+++ src/tpiproyecto2/TPIPROYECTO2.java
@@ -17,8 +17,8 @@ public class TPIPROYECTO2 {
      */
     public static void main(String[] args) {
     Controlador controlador = new Controlador();
-    controlador.cargarEquipos("C:\\Users\\mateo\\OneDrive\\Desktop\\TPI\\TPIPROYECTO2\\src\\Archivos\\EquiposMundial.txt");     
-    controlador.cargarJugadores("C:\\Users\\mateo\\OneDrive\\Desktop\\TPI\\TPIPROYECTO2\\src\\Archivos\\jugadoresMundial.txt"); 
+    controlador.cargarEquipos("src\\Archivos\\EquiposMundial.txt");     
+    controlador.cargarJugadores("src\\Archivos\\jugadoresMundial.txt"); 
     controlador.iniciar();
     }
 }
